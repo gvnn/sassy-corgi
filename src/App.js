b5819843fb49b0a53e75e2ae6d97a27eb837d1c1
Giovanni Ferron <gvnnrulez@gmail.com>
2017-10-12 21:33:04 +0100

    Add slide 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 
 class App extends Component {
+  handlePrefixChange() {
+    const element = document.getElementsByTagName("body")[0];
+    element.classList.toggle("blue");
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">sassy-corgi</h1>
+      <div className="application">
+        <header className="application__header">
+          <img src={logo} className="application__logo" alt="logo" />
+          <h1 className="application__title">sassy-corgi</h1>
         </header>
+        <div className="content">
+          <h3>1. Prefixing parent selector references</h3>
+          <p>
+            <a onClick={this.handlePrefixChange}>I'm a link that should be red but with a "blue" body I change</a>
+          </p>
+        </div>
       </div>
     );
   }
